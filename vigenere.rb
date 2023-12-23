58626e53f8974e641183a317cd7d806f9cb01afd
ShaBel7 <sbelmont@ucol.mx>
2023-12-23 16:08:53 -0600

    vigenere new

--- vigenere.rb
+++ vigenere.rb
@@ -4,41 +4,47 @@
 # frozen_string_literal: true
 # Vigenere Algorithm
 
+# frozen_string_literal: true
+
+# Vigenere Algorithm
 class Encryptor
-  def initialize(original_message, key)
-    @original_message = original_message.upcase
-    @key = key.upcase
-  end
-
-  def encrypt
-    encrypted_message = ''
-    @original_message.each_char.with_index do |char, index|
-      original_value = char.ord - 'A'.ord
-      key_value = key_value_at(index)
-      encrypted_char = encrypt_char(original_value, key_value)
-      encrypted_message += encrypted_char
+    def initialize(original_message, key)
+      @original_message = original_message.upcase
+      @key = key.upcase
     end
-    encrypted_message
+  
+    def encrypt
+      encrypted_message = ''
+      @original_message.each_char.with_index do |char, index|
+        original_value = char.upcase.ord - 'A'.ord
+        key_value = key_value_at(index)
+        encrypted_char = encrypt_char(original_value, key_value)
+        puts "Char: #{char}, Original Value: #{original_value}, Key Value: #{key_value}, Encrypted Char: #{encrypted_char}"
+        encrypted_message += encrypted_char
+      end
+      encrypted_message
+    end
+  
+    private
+  
+    def key_value_at(index)
+      @key[index % @key.length].upcase.ord - 'A'.ord
+    end
+  
+    def encrypt_char(original_value, key_value)
+      encrypted_value = (original_value + key_value) % 26
+      encrypted_char = (encrypted_value + 'A'.ord).chr
+      puts "Original Value: #{original_value}, Key Value: #{key_value}, Encrypted Value: #{encrypted_value}, Encrypted Char: #{encrypted_char}"
+      encrypted_char
+    end
   end
-
-  private
-
-  def key_value_at(index)
-    @key[index % @key.length].ord - 'A'.ord
-  end
-
-  def encrypt_char(original_value, key_value)
-    encrypted_value = (original_value + key_value) % 26
-    (encrypted_value + 'A'.ord).chr
-  end
-end
-
-# Example usage:
-original_message = 'HOLA'
-key = 'SECRETO'
-encryptor = Encryptor.new(original_message, key)
-encrypted_message = encryptor.encrypt
-
-puts "Original message: #{original_message}"
-puts "Key: #{key}"
-puts "Encrypted message: #{encrypted_message}"
+  
+  # Example usage:
+  original_message = 'HOLA'
+  key = 'SECRETO'
+  encryptor = Encryptor.new(original_message, key)
+  encrypted_message = encryptor.encrypt
+  
+  puts "Original message: #{original_message}"
+  puts "Key: #{key}"
+  puts "Encrypted message: #{encrypted_message}"
